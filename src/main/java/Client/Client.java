36d8d84c022025a89edbe4eb8f02d332570f1a1f
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-06-12 23:30:26 +0330

    reformat code

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,4 +1,4 @@
-package Utilities;
+package Client;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -6,24 +6,24 @@ import java.net.Socket;
 
 public class Client {
     public static void joinGame() throws IOException {
-        try{
-            Socket socket1=new Socket("localhost",3737);
+        try {
+            Socket socket1 = new Socket("localhost", 3737);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket1.getOutputStream());
             objectOutputStream.writeObject("player join the game");
-            objectOutputStream.flush();}
-        catch(IOException e){
+            objectOutputStream.flush();
+        } catch (IOException e) {
         }
     }
 
 
-    public static void createGame(){
-        try{
-            Socket socket1=new Socket("localhost",3737);
+    public static void createGame() {
+        try {
+            Socket socket1 = new Socket("localhost", 3737);
             // It is the responsibility of the server to check the matters related to making the game
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket1.getOutputStream());
             objectOutputStream.writeObject("player join the game");
-            objectOutputStream.flush();}
-        catch(IOException e){
+            objectOutputStream.flush();
+        } catch (IOException e) {
         }
     }
 }
---
+++ src/main/java/Client/Player.java
@@ -0,0 +1,14 @@
+package Client;
+
+import Utilities.Card;
+
+public class Player extends Client {
+    public static void showWinSets() {
+
+    }
+
+    public static Card putCard(Card card) {
+        return card;
+    }
+
+}
