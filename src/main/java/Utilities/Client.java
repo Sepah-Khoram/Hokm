bd6a94d547ad3d0c1235fe0cdf658c19198e060e
mosalehrabbani <155375059+mosalehrabbani@users.noreply.github.com>
2024-06-12 15:54:47 +0330

    add client

---
+++ src/main/java/Utilities/Client.java
@@ -0,0 +1,29 @@
+package Utilities;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client {
+    public static void joinGame() throws IOException {
+        try{
+            Socket socket1=new Socket("localhost",3737);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket1.getOutputStream());
+            objectOutputStream.writeObject("player join the game");
+            objectOutputStream.flush();}
+        catch(IOException e){
+        }
+    }
+
+
+    public static void creategame(){
+        try{
+            Socket socket1=new Socket("localhost",3737);
+            // It is the responsibility of the server to check the matters related to making the game
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket1.getOutputStream());
+            objectOutputStream.writeObject("player join the game");
+            objectOutputStream.flush();}
+        catch(IOException e){
+        }
+    }
+}
