8d118ed8863cbb7df140ecf747546f09c634d606
Amirhoseinhmd83 <amirhosein.hamidii83@gmail.com>
2024-06-14 04:59:59 +0330

    add class GameService

--- src/main/java/Utilities/GameService.java
+++ src/main/java/Utilities/GameService.java
@@ -7,7 +7,9 @@ import java.util.List;
 public class GameService {
     public static Card[][] divideCards(int numberOfPlayers) {
         List<Card> cards = new ArrayList<>(52);
-
+        if (numberOfPlayers != 2 && numberOfPlayers != 4) {
+            throw new IllegalArgumentException("Number of invalid players!!-->>Must be 2 or 4 players.");
+        }
         int count = 0; // number of cards
 
         // populate deck with Card objects
@@ -17,7 +19,16 @@ public class GameService {
 
         Collections.shuffle(cards); // shuffle deck
 
-        Card[][] dividedList = new Card[numberOfPlayers][13];
+        int cardsPerPlayer = 52 / numberOfPlayers;
 
+        Card[][] dividedList = new Card[numberOfPlayers][13];
+        for (int i = 0; i < numberOfPlayers ; i++) {
+            for (int j = 0; j < cardsPerPlayer ; j++) {
+
+                dividedList[i][j] = cards.get(i * cardsPerPlayer + j);
+
+            }
+        }
+        return dividedList;
     }
 }
