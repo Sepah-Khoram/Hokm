0382acba5b4f62a242caef9d2db75ad0d4e66984
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-06-14 08:33:31 +0330

    change method divideCards

--- src/main/java/Utilities/GameService.java
+++ src/main/java/Utilities/GameService.java
@@ -5,11 +5,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class GameService {
-    public static Card[][] divideCards(int numberOfPlayers) {
-        List<Card> cards = new ArrayList<>(52);
+    public static Card[][] divideCards(int numberOfPlayers) throws IllegalArgumentException {
         if (numberOfPlayers != 2 && numberOfPlayers != 4) {
-            throw new IllegalArgumentException("Number of invalid players!!-->>Must be 2 or 4 players.");
+            throw new IllegalArgumentException(
+                    "Invalid number of players!!-->>Must be 2 or 4 players.");
         }
+
+        List<Card> cards = new ArrayList<>(52); // deck of cards
+
         int count = 0; // number of cards
 
         // populate deck with Card objects
@@ -19,16 +22,22 @@ public class GameService {
 
         Collections.shuffle(cards); // shuffle deck
 
-        int cardsPerPlayer = 52 / numberOfPlayers;
-
         Card[][] dividedList = new Card[numberOfPlayers][13];
-        for (int i = 0; i < numberOfPlayers ; i++) {
-            for (int j = 0; j < cardsPerPlayer ; j++) {
 
-                dividedList[i][j] = cards.get(i * cardsPerPlayer + j);
-
+        if (numberOfPlayers == 2) {
+            for (int i = 0, j = 0; i < 52; i += 4, j++) {
+                dividedList[0][i] = cards.get(j);
+                dividedList[1][i + 2] = cards.get(j);
+            }
+        } else {
+            for (int i = 0, j = 0; i < 52; i += 4, j++) {
+                dividedList[0][i] = cards.get(j);
+                dividedList[1][i] = cards.get(j);
+                dividedList[2][i] = cards.get(j);
+                dividedList[3][i] = cards.get(j);
             }
         }
+
         return dividedList;
     }
 }
