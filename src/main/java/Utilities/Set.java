bdda0eb86969add17906c589e5bfef82c6ad6cca
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-06-15 12:21:13 +0330

    improve class set

--- src/main/java/Utilities/Set.java
+++ src/main/java/Utilities/Set.java
@@ -1,6 +1,22 @@
 package Utilities;
 
+import Server.Player;
+
+import java.security.SecureRandom;
+
 public class Set {
-    String result;
-    Team winner;
+    private String result;
+    private Team winner;
+    private final Player ruler;
+    private int round;
+
+    public Set(Player[] players) {
+        round = 0;
+        int dealer = new SecureRandom().nextInt(0, players.length); // select dealer
+        this.ruler = players[dealer];
+    }
+
+    public Player getRuler() {
+        return ruler;
+    }
 }
