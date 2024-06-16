11b52c53e0dbb534e93bdf940fba7fab792c66b4
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-06-17 02:01:06 +0330

    add logging and wait thread of players for start the game

--- src/main/java/Server/Game.java
+++ src/main/java/Server/Game.java
@@ -6,17 +6,23 @@ import Utilities.Set;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Game implements Runnable {
     private final Player[] players;
     private final UUID token;
-    private final ExecutorService executorService;
+    private static final Logger logger = LoggerManager.getLogger(); // logger for logging
+    private final CyclicBarrier gameStartBarrier; // for wait threads for the game to start
     private final CopyOnWriteArrayList<Player> connectedPlayers;
-    private final ArrayList<Set> sets;
+    private final ExecutorService executorService; // manage threads
+    private final List<Set> sets;
     private Set currentSet;
     private boolean isGameStarted;
 
@@ -26,7 +32,11 @@ public class Game implements Runnable {
         connectedPlayers = new CopyOnWriteArrayList<>();
         sets = new ArrayList<>();
         executorService = Executors.newFixedThreadPool(numberOfPlayers);
+        gameStartBarrier = new CyclicBarrier(numberOfPlayers, this::startGame);
         isGameStarted = false;
+
+        logger.info("Game created with token " + token + " for " +
+                numberOfPlayers + " players."); // logging create game
 
         addPlayer(player); // add player 1
     }
@@ -40,21 +50,26 @@ public class Game implements Runnable {
             player.setPlayerNumber(connectedPlayers.size()); // set player number
             executorService.execute(player);
 
-            if (connectedPlayers.size() == getNumberOfPlayers()) {
-                executorService.shutdown();
-                startGame();
+            logger.info("Player " + player.getId() + " added to the game. Player number: "
+                    + player.getPlayerNumber());
+
+            if (connectedPlayers.size() == players.length) {
+                logger.info("All players have joined the game.");
             }
+        } else {
+            logger.warning("Attempted to add a player to a full game.");
         }
     }
 
     private void startGame() {
+        logger.info("All players have joined. Game is starting.");
         /*
          implement check name
         */
         // send id and name of players in the game
         sendData("players:");
         for (Player player : players) {
-            sendData(player.getName() + ":" + player.getId());
+            sendData(player.getId() + ":" + player.getName());
         }
 
         System.out.println("Game " + token + " started with " + players.length + " players.");
@@ -65,6 +80,7 @@ public class Game implements Runnable {
         for (Player player : players) {
             player.sendData(data);
         }
+        logger.info("Data sent to all players: " + data);
     }
 
     @Override
@@ -74,6 +90,7 @@ public class Game implements Runnable {
                 Thread.sleep(100); // Wait for a short period before checking again
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+                logger.log(Level.SEVERE, "Game thread interrupted while waiting to start", e);
             }
         }
 
@@ -85,13 +102,16 @@ public class Game implements Runnable {
             sendData("set:" + sets.size()); // send set number to the clients
             sendData("ruler:" + currentSet.getRuler().getId()); // get ruler of this set
 
+            logger.info("New set started. Set number: " + sets.size());
+            logger.info("Ruler of the set: " + currentSet.getRuler().getId());
+
             // devide cards btw users and send them to users
             Card[][] cards = GameService.divideCards(getNumberOfPlayers());
             for (int i = 0; i < players.length; i++) {
                 players[i].setCards(Arrays.asList(cards[i]));
+                logger.info("Cards dealt to player " +
+                        players[i].getPlayerNumber() + ": " + Arrays.toString(cards[i]));
             }
-
-
         }
     }
 
@@ -119,5 +139,10 @@ public class Game implements Runnable {
 
     public void setRule(Card.Suit rule) {
         currentSet.setRule(rule);
+        logger.info("Rule set for the current set: " + rule);
+    }
+
+    public CyclicBarrier getBarrier() {
+        return gameStartBarrier;
     }
 }
