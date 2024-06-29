5df2430f3449b88d707c399a3d359d933afd8fea
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-06-29 22:26:27 +0330

    optimize the code in method compareTo and enum Face

--- src/main/java/Utilities/Card.java
+++ src/main/java/Utilities/Card.java
@@ -1,6 +1,8 @@
 // class to represent a Card in a deck of cards
 package Utilities;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
 
 public class Card implements Comparable<Card>, Serializable {
@@ -14,14 +16,8 @@ public class Card implements Comparable<Card>, Serializable {
     }
 
     @Override
-    public int compareTo(Card card) {
-        if (this.face.getCode() == 1)
-            return 14 - card.face.getCode();
-
-        if (card.face.getCode() == 1)
-            return this.face.getCode() - 14;
-
-        return this.face.getCode() - card.face.getCode();
+    public int compareTo(@NotNull Card card) {
+        return suit.compareTo(card.getSuit());
     }
 
     @Override
@@ -34,19 +30,7 @@ public class Card implements Comparable<Card>, Serializable {
     }
 
     public enum Face {
-        Ace(1), Deuce(2), Three(3), Four(4), Five(5),
-        Six(6), Seven(7), Eight(8), Nine(9), Ten(10),
-        Jack(11), Queen(12), King(13);
-
-        private final int code;
-
-        Face(int code) {
-            this.code = code;
-        }
-
-        public int getCode() {
-            return code;
-        }
+        Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
     }
 
     public enum Suit {Clubs, Diamonds, Hearts, Spades}
