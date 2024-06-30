861762e8f5ef8b67e984e8f545b627ec96ffeb3e
mosalehrabbani <155375059+mosalehrabbani@users.noreply.github.com>
2024-06-30 13:19:04 +0330

    start the server manager

--- src/main/java/Server/ServerManager.java
+++ src/main/java/Server/ServerManager.java
@@ -1,9 +1,22 @@
 package Server;
 
+import java.util.Scanner;
+
 public class ServerManager implements Runnable{
 
     @Override
     public void run() {
-
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("1. Show all games \n2. Show details of the selected game \n3. Loging the information" );
+        System.out.println("insert your Choose :");
+        switch (scanner.nextInt()){
+            case 1:
+                //
+            case 2:
+                //
+            case 3:
+                //
+            default:
+        }
     }
 }
