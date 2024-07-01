0aaff48b09f413bddaca03b5e6074f621362ad53
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-07-01 09:42:23 +0330

    add method getPlayers

--- src/main/java/Server/Team.java
+++ src/main/java/Server/Team.java
@@ -15,7 +15,11 @@ public class Team {
         return (players.getFirst().getId() + "," + players.getLast().getId());
     }
 
-    public void addSet(Set newSet) {
+    void addSet(Set newSet) {
         this.sets.add(newSet);
+    }
+
+    ArrayList<Player> getPlayers() {
+        return players;
     }
 }
